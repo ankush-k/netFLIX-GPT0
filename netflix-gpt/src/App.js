fd783a688932153a3e849ae72c2193673e3ab8dc
ankush-k <ankush.khaparde@gmail.com>
2023-12-19 14:55:05 +0530

    initial upload with tailwind css

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="font-extrabold text-lg text-green-500">
+      Let's Start with Netflix
+    </div>
+  );
+}
+
+export default App;
